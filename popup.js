27bc8c5f46d655e67026525533166967259ba8c3
EShrestha <eshrestha961@gmail.com>
2023-01-25 01:06:06 -0700

    Designed home page, added icons

--- popup.js
+++ popup.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
     console.log("Opened popup.")
+    openAnimate();
 });
+
+const openAnimate = () => {
+    console.log("Animate ran.")
+    document.getElementsByTagName("html")[0].style.transition =
+    //`width 400ms cubic-bezier(0.18, 0.89, 0.05, 1.51),
+    //height 400ms cubic-bezier(0.18, 0.89, 0.05, 1.51)
+    `width 350ms ease-in,
+    height 350ms ease-in
+    `;
+
+    // Animate expand of popup to these units
+    document.getElementsByTagName("html")[0].style.width="100px";
+    document.getElementsByTagName("html")[0].style.height="100px";
+}
