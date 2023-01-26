4553a140ddf52191176ced2598328e894605f3ce
EShrestha <eshrestha961@gmail.com>
2023-01-25 17:28:22 -0700

    Added funcunality to go back to home page

--- Text/text.js
+++ Text/text.js
@@ -36,3 +36,24 @@ sendBtn.addEventListener('click', () => {
     
     }, 100);
 })
+
+backBtn.addEventListener('click', async () => {
+    console.log("Back!");
+    console.log("Animate ran.")
+    document.getElementsByTagName("html")[0].style.transition =
+        `width 250ms ease-in,
+    height 250ms ease-in
+    `;
+
+    await setTimeout(() => {
+        // Animate expand of popup to these units
+        outputDiv.style.width="0px";
+        outputDiv.style.height="0px";
+        document.getElementsByTagName("html")[0].style.width = "100px";
+        document.getElementsByTagName("html")[0].style.height = "100px";
+    }, 100);
+
+    setTimeout(() => {
+        window.location.pathname = "../popup.html";
+    }, 350)
+});
